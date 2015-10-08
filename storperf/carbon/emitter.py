003e39f7afa940febcb93a6b16b86893c5dd97db
mbeierl <mark.beierl@emc.com>
2015-10-07 19:19:11 -0700

    Creation of emitters
    JIRA: STORPERF-7 
    
    Change-Id: I0a34af62ef850c9fc3e117b5c53283fc8d17561f
    Signed-off-by: mbeierl <mark.beierl@emc.com>

---
+++ storperf/carbon/emitter.py
@@ -0,0 +1,28 @@
+##############################################################################
+# Copyright (c) 2015 EMC and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+import socket
+
+class CarbonMetricTransmitter():
+    
+    carbon_host = '127.0.0.1'
+    carbon_port = 2003
+    
+    def __init__(self):
+        pass
+    
+    def transmit_metrics(self, metrics):
+        self.carbon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.carbon_socket.connect((self.carbon_host, self.carbon_port))
+        
+        for key, metric in metrics.items():
+            message = key + " " + metric + "\n"
+            self.carbon_socket.send(message)
+            
+        self.carbon_socket.close()
