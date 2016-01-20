c5f233d3ab0818d8dd598d835742db2ec4c3a890
Mark Beierl <mark.beierl@emc.com>
2016-01-19 20:58:35 -0500

    Remote slave agent workload
    Add storperf master object to manage stack
    lifecycle.
    Add configuration db.
    Creation of CLI vs. main so that ReST API
    and CLI API can be kept clear.
    Fixed License in files.
    Changed DB objects to be thread safe.
    Added ssh server to container if desired
    for CLI.
    
    Change-Id: Idfe84bfb7920e6ce19d27462593c21ea86e7b406
    JIRA: STORPERF-29
    Signed-off-by: Mark Beierl <mark.beierl@emc.com>
    (cherry picked from commit 488a47d945d3ef3dfa9ee37ca0aac3b480ffc800)

--- ci/setup.py
+++ ci/setup.py
@@ -1,3 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 EMC and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 from setuptools import setup, find_packages
 
 
@@ -16,6 +25,7 @@ setup(
                       "flake8>=2.5.1",
                       "flask>=0.10.1",
                       "flask-restful>=0.3.5",
+                      "html2text>=2016.1.8",
                       "mock>=1.3",
                       "pyyaml>=3.11",
                       "python-cinderclient>=1.5.0",
