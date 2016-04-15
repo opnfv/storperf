07a375f25ee831100ecf21e7bb9c1bdcd3b960f5
Mark Beierl <mark.beierl@emc.com>
2016-04-15 13:10:47 -0400

    Tests in verify
    Run the tests in the verify command instead
    of just merge so that way patches can be
    rejected if tests fail
    
    Change-Id: I8907618a324296460a71a51d087926f9089e0d21
    Signed-off-by: Mark Beierl <mark.beierl@emc.com>

--- ci/verify.sh
+++ ci/verify.sh
@@ -10,6 +10,44 @@
 
 echo "Verifying code format and compliance..."
 
-if [ -x /usr/bin/flake8 ] ; then
-	flake8 storperf
+if [ -z $WORKSPACE ]
+then
+    WORKSPACE="$HOME"
 fi
+
+virtualenv $WORKSPACE/storperf_venv
+source $WORKSPACE/storperf_venv/bin/activate
+
+pip install setuptools
+pip install coverage==4.0.3
+pip install flask==0.10
+pip install flask-restful==0.3.5
+pip install funcsigs==0.4
+pip install flake8==2.5.4
+pip install html2text==2016.1.8
+pip install mock==1.3.0
+pip install nose==1.3.7
+pip install python-cinderclient==1.6.0
+pip install python-glanceclient==1.1.0
+pip install python-heatclient==0.8.0
+pip install python-keystoneclient==1.6.0
+pip install python-neutronclient==2.6.0
+pip install python-novaclient==2.28.1
+pip install pyyaml==3.10
+pip install requests==2.9.1
+pip install six==1.10.0
+
+python ci/setup.py develop
+
+flake8 storperf
+
+nosetests --with-xunit \
+         --with-coverage \
+         --cover-package=storperf\
+         --cover-xml \
+         storperf
+rc=$?
+
+deactivate
+
+exit $rc
