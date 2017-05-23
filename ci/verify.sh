bde628b56fc7c01f823fd76a10e97ca899632849
mbeierl <mark.beierl@dell.com>
2017-05-23 14:51:26 -0400

    Specify numpy version
    Adds explicit version of numpy so it cannot conflict with any other that
    may be present in the system path.
    
    Change-Id: I23b21bca43f55805b3087646b1c31e1aea686aca
    JIRA: STORPERF-126
    Signed-off-by: mbeierl <mark.beierl@dell.com>

--- ci/verify.sh
+++ ci/verify.sh
@@ -30,6 +30,7 @@ pip install funcsigs==0.4
 pip install flake8==2.5.4
 pip install html2text==2016.1.8
 pip install keystoneauth1==2.12.1
+pip install numpy==1.6
 pip install matplotlib==1.3.1
 pip install mock==1.3.0
 pip install nose==1.3.7
