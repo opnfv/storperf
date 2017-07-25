08b6c703d85d60433977529981447e513ab78941
saksham115 <saksham.agrawal@research.iiit.ac.in>
2017-07-25 23:09:16 +0530

    Fixing URL for static files Earlier the static files were being served on /static/ now it has been fixed and are being served on /reporting/js/ and /reporting/css/ for js and css files respectively. JIRA: STORPERF-192
    Change-Id: Ibcce2210de88f2359d011752513fdab9f930ab5b
    Signed-off-by: saksham115 <saksham.agrawal@research.iiit.ac.in>

--- docker/storperf-reporting/src/app.py
+++ docker/storperf-reporting/src/app.py
@@ -7,19 +7,17 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask, redirect, url_for, request, render_template, session, send_from_directory
 import urllib
 import json
 app = Flask(__name__)
 app.secret_key = 'storperf_graphing_module'
-
 
 @app.route('/reporting/success/')
 def success():
     data = urllib.urlopen(session["url"]).read()
     data = json.loads(data)
     return render_template('plot_tables.html', data=data)
-
 
 @app.route('/reporting/url', methods=['POST', 'GET'])
 def url():
@@ -28,11 +26,17 @@ def url():
         session["url"] = url
         return redirect(url_for('success'))
 
+@app.route('/reporting/js/<path:path>')
+def js(path):
+    return send_from_directory('static/js/', path)
+
+@app.route('/reporting/css/<path:path>')
+def css(path):
+    return send_from_directory('static/css/', path)
 
 @app.route('/reporting/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, threaded=True)
