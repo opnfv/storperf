f60607c9ff830546b8682bdfa67a2cbacd5f28d0
Shrenik <shrenik.jain@research.iiit.ac.in>
2017-08-16 17:04:00 +0530

    Graphite Standalone container
    A new Graphite container is used
    Metrics are sent to both Graphite instances
    However, it seems that some metrics might be missing
    This is however a direct plugin. There are differences in
    carbon.conf, storage-schemas.conf and other files as well.
    It is suggested to write own Dockerfile instead of using
    the image available. We anyway have to do it with respect
    ARM Support.
    
    Change-Id: Id34c728f598150caac23ac167c3cce5eaf183a6c
    JIRA: STORPERF-142
    Signed-off-by: Shrenik <shrenik.jain@research.iiit.ac.in>
    Signed-off-by: mbeierl <mark.beierl@dell.com>

--- docker/storperf-master/storperf/db/graphite_db.py
+++ docker/storperf-master/storperf/db/graphite_db.py
@@ -9,19 +9,29 @@
 
 import json
 import logging
-
 import requests
-
-from storperf.db.job_db import JobDB
 
 
 class GraphiteDB(object):
 
+    graphite_host = "storperf-graphite"
+    graphite_port = 8080
+
     def __init__(self):
-        """
-        """
-        self._job_db = JobDB()
         self.logger = logging.getLogger(__name__)
+
+    def fetch_item(self, target):
+
+        result = None
+        request = ("http://%s:%s/render/?format=json&target=%s"
+                   % (self.graphite_host, self.graphite_port, target))
+        self.logger.debug("Calling %s" % (request))
+
+        response = requests.get(request)
+        if (response.status_code == 200):
+            result = json.loads(response.content)
+
+        return result
 
     def fetch_series(self, workload, metric, io_type, time, duration):
 
@@ -29,13 +39,14 @@ class GraphiteDB(object):
         end = time
         start = end - duration
 
-        request = ("http://127.0.0.1:8000/render/?target="
+        request = ("http://%s:%s/render/?target="
                    "averageSeries(%s.*.jobs.1.%s.%s)"
                    "&format=json"
                    "&from=%s"
-                   "&until=%s" %
-                   (workload, io_type, metric,
-                    start, end))
+                   "&until=%s"
+                   % (self.graphite_host, self.graphite_port,
+                      workload, io_type, metric,
+                      start, end))
         self.logger.debug("Calling %s" % (request))
 
         response = requests.get(request)
