ab9efc4abe10b61eb8d7825d1b40c452c2dca2e9
mbeierl <mark.beierl@dell.com>
2017-09-12 16:34:24 -0400

    Add Multi Arch to Docker
    Adds multi arch support to the launching of the
    containers.
    
    Change-Id: Iee89cfad3dc455fe8fdd7861d73fadbe314c2c1e
    JIRA: STORPERF-220
    Signed-off-by: mbeierl <mark.beierl@dell.com>

--- ci/launch_docker_container.sh
+++ ci/launch_docker_container.sh
@@ -8,32 +8,32 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-cd `dirname $0`
-ci=`pwd`
+cd "$(dirname "$0")" || exit 1
+ci=$(pwd)
 
-cd ${ci}/../docker-compose
+cd "${ci}/../docker-compose" || exit 1
 
-export ENV_FILE=${ci}/job/admin.rc
-export CARBON_DIR=${ci}/job/carbon/
+export ENV_FILE="${ci}/job/admin.rc"
+export CARBON_DIR="${ci}/job/carbon/"
 
-if [ ! -d ${ci}/job/carbon ]
+if [ ! -d "${ci}/job/carbon" ]
 then
-    mkdir ${ci}/job/carbon
+    mkdir -p "${ci}/job/carbon"
 fi
 
-if [ -z ${ARCH} ]
+if [ -z "${ARCH}" ]
 then
-    ARCH=x86_64
+    ARCH=$(uname -m)
 fi
 
 export ARCH
 
-if [ -z ${DOCKER_TAG} ]
+if [ -z "${DOCKER_TAG}" ]
 then
     DOCKER_TAG=latest
 fi
 
-export TAG=${DOCKER_TAG}
+export TAG="${ARCH}-${DOCKER_TAG}"
 
 docker-compose pull
 docker-compose up -d
@@ -42,7 +42,7 @@ echo "Waiting for StorPerf to become active"
 
 ATTEMPTS=20
 
-while [ $(curl -s -o /dev/null -I -w "%{http_code}" -X GET http://127.0.0.1:5000/api/v1.0/configurations) != "200" ]
+while [ "$(curl -s -o /dev/null -I -w '%{http_code}' -X GET http://127.0.0.1:5000/api/v1.0/configurations)" != "200" ]
 do
     ATTEMPTS=$((ATTEMPTS - 1))
     if [ ${ATTEMPTS} -le 1 ]
