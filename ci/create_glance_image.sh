41a5131bf1ddd32ee7fbefeca06ecd31f8709ae6
mbeierl <mark.beierl@dell.com>
2017-09-20 19:45:45 -0400

    Adding FUEL Support
    Changes to Ubuntu 17.04 to overcome issue in 4.4
    kernel on ARM for hotplug of cinder volumes.
    
    Adds support for FUEL MCP certificate for SSL
    communication with OpenStack.
    
    Hard codes the target device to /dev/vdc for FUEL
    as /dev/vdb is reserved for the OpenStack config
    drive.
    
    Change-Id: I17dae23f455448f632e0921dbdae45824bbeaa61
    Signed-off-by: mbeierl <mark.beierl@dell.com>

--- ci/create_glance_image.sh
+++ ci/create_glance_image.sh
@@ -8,9 +8,15 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+cd "$(dirname "$0")" || exit
+if [ ! -d job ]
+then
+    mkdir job
+fi
+
 ARCH="${ARCH:-$(uname -m)}"
 
-IMAGE_NAME="Ubuntu 16.04 ${ARCH}"
+IMAGE_NAME="Ubuntu 17.04 ${ARCH}"
 
 echo "Checking for ${IMAGE_NAME} in Glance"
 
@@ -21,14 +27,14 @@ then
 
     case "${ARCH}" in
     aarch64)
-        FILE=ubuntu-16.04-server-cloudimg-arm64-uefi1.img
+        FILE=ubuntu-17.04-server-cloudimg-arm64.img
         PROPERTIES="--property hw_firmware_type=uefi --property hw_video_model=vga"
         ;;
     armhf)
-        FILE=ubuntu-16.04-server-cloudimg-armhf-disk1.img
+        FILE=ubuntu-17.04-server-cloudimg-armhf.img
         ;;
     x86_64)
-        FILE=ubuntu-16.04-server-cloudimg-amd64-disk1.img
+        FILE=ubuntu-17.04-server-cloudimg-amd64.img
         ;;
     *)
         echo "Unsupported architecture: ${ARCH}"
@@ -36,10 +42,12 @@ then
         ;;
     esac
 
-    wget --continue -q "https://cloud-images.ubuntu.com/releases/16.04/release/${FILE}"
+    wget --continue -q "https://cloud-images.ubuntu.com/releases/17.04/release/${FILE}"
     openstack image create "${IMAGE_NAME}" --disk-format qcow2 --public \
     ${PROPERTIES} \
     --container-format bare --file "${FILE}"
 fi
 
 openstack image show "${IMAGE_NAME}"
+
+echo "${IMAGE_NAME}" > job/glance_image_name
